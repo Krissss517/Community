0a2bd03e4476891f2eb2dbb021955bd74752a8af
Kris088794 <63542929+Kris088794@users.noreply.github.com>
2020-07-20 16:16:32 +0800

    add flyway

---
+++ src/main/java/com/example/community/migrate/BaselineOnMigrate.java
@@ -0,0 +1,11 @@
+package com.example.community.migrate;
+
+import org.springframework.boot.autoconfigure.flyway.FlywayMigrationStrategy;
+
+public class BaselineOnMigrate {
+    /*@Override
+    public void migrate(org.flywaydb.core.Flyway flyway) {
+        flyway.setBaselineOnMigrate(true);
+        flyway.migrate();
+    }*/
+}
